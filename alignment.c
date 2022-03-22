0c64cf197dbdd0dd330628ad6b05dc104bbfbeb1
Judit <szenei.judit@gmail.com>
2022-03-22 08:13:32 +0100

    cleaned code

--- alignment.c
+++ alignment.c
@@ -9,6 +9,6 @@ int main(void){
     unsigned char bad_buff[sizeof(struct S)];
     _Alignas(struct S) unsigned char good_buff[sizeof(struct S)];
 
-    struct S *bad_s_ptr=(struct S *)bad_buff;   //wrong pointer alignment
-    struct S *good_s_ptr=(struct S *)good_buff; //good pointer alignment    
+    struct S *bad_s_ptr = (struct S *)bad_buff;   //wrong pointer alignment
+    struct S *good_s_ptr = (struct S *)good_buff; //good pointer alignment    
 }
