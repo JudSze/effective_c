cda85da2a66ed9c51003bb9aa7f5fecc91c98fdd
Judit <szenei.judit@gmail.com>
2022-03-22 08:12:37 +0100

    cleaning code

--- increment.c
+++ increment.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 void increment(void){
-    static unsigned int counter=0; //has to have constant value, can only hold positive numbers
+    static unsigned int counter = 0; //has to have constant value, can only hold positive numbers
     counter++;
     printf("%d\t", counter);
 }
 
 int main(void){
-    for (int i=0; i<5; i++){
+    for (int i = 0; i < 5; i++){
         increment();
     }
+    
     return 0;
 }
--- swap.c
+++ swap.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 void swap(int *a, int *b){
-    int t=*a;
-    *a=*b;
-    *b=t;
-    printf("swap: a=%d, b=%d\n", *a, *b);
+    int t = *a;
+    *a = *b;
+    *b = t;
+    printf("swap: a = %d, b = %d\n", *a, *b);
 }
 
 int main(void){
-    int a=20;
-    int b=10;
+    int a = 20;
+    int b = 10;
 
-    printf("main: a=%d, b=%d\n",a,b);
+    printf("main: a = %d, b = %d\n",a,b);
     swap(&a,&b);
+    
     return 0;
 }
